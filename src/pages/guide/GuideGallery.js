5d322202375979c029c0271657e3516825b8ffb0
yerin <byl06@naver.com>
2025-04-23 11:48:07 +0900

    배포용 url 재수정

--- src/pages/guide/GuideGallery.js
+++ src/pages/guide/GuideGallery.js
@@ -63,7 +63,7 @@ const GuideGallery = () => {
 
         try {
             // 1. 먼저 첫 페이지를 요청해서 총 페이지 수 알아냄
-            const firstPageUrl = `${apiBaseUrl}/api/jeju-festival?apiKey=${apiBaseUrl}&locale=kr&category=${category}&page=1&pageSize=${pageSize}`;
+            const firstPageUrl = `${apiBaseUrl}/api/jeju-festival?&locale=kr&category=${category}&page=1&pageSize=${pageSize}`;
             const { data: firstPageData } = await axios.get(firstPageUrl);
 
             allItems = [...firstPageData.items];
@@ -73,7 +73,7 @@ const GuideGallery = () => {
             const pagePromises = [];
 
             for (let page = 2; page <= totalPages; page++) {
-                const url = `${apiBaseUrl}/api/jeju-festival?apiKey=${apiBaseUrl}&locale=kr&category=${category}&page=${page}&pageSize=${pageSize}`;
+                const url = `${apiBaseUrl}/api/jeju-festival&locale=kr&category=${category}&page=${page}&pageSize=${pageSize}`;
                 pagePromises.push(axios.get(url));
             }
 
